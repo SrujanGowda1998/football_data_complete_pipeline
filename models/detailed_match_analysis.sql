eb5f090f0bf9eece8a31a5ce8011061c2de8151d
Arpita Krishna Bhat <arpitakrishna.bhat@srh-heidelberg.org>
2024-05-05 20:05:47 +0000

    Final cleaning done

--- models/detailed_match_analysis.sql
+++ models/detailed_match_analysis.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table', table_name = 'detailed_match_analysis') }}
+
 WITH match_referee_performance AS (
     SELECT 
         m.match_id,
@@ -12,18 +14,14 @@ WITH match_referee_performance AS (
              WHEN m.fulltime_home_goals < m.fulltime_away_goals THEN 'Away Win'
              ELSE 'Draw' END AS match_outcome
     FROM {{ ref('intermediate_match_outcomes4') }} m
-),
-
-referee_stats AS (
-    SELECT 
-        main_referee,
-        referee_nationality,
-        COUNT(match_id) AS total_matches,
-        COUNT(CASE WHEN match_outcome = 'Home Win' THEN 1 END) AS home_wins,
-        COUNT(CASE WHEN match_outcome = 'Away Win' THEN 1 END) AS away_wins,
-        COUNT(CASE WHEN match_outcome = 'Draw' THEN 1 END) AS draws
-    FROM match_referee_performance
-    WHERE main_referee IS NOT NULL AND referee_nationality IS NOT NULL
-    GROUP BY main_referee, referee_nationality
 )
 
+SELECT 
+    main_referee,
+    referee_nationality,
+    COUNT(match_id) AS total_matches,
+    COUNT(CASE WHEN match_outcome = 'Home Win' THEN 1 ELSE NULL END) AS home_wins,
+    COUNT(CASE WHEN match_outcome = 'Away Win' THEN 1 ELSE NULL END) AS away_wins,
+    COUNT(CASE WHEN match_outcome = 'Draw' THEN 1 ELSE NULL END) AS draws
+FROM match_referee_performance
+GROUP BY main_referee, referee_nationality
